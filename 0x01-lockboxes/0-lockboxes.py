d319abe7502e91e72dde04383f41b100bde49813
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2024-10-11 21:02:31 +0000

    test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,25 +3,16 @@
 Defines a function canUnlockAll that determines if a box can be unlock
 """
 
-
 def canUnlockAll(boxes):
-    """
-    A  method that determines if all the boxes can be opened.
-    Args:
-        boxes (list of boxes)
-    Returns:
-        bool: True if all boxes can be unlock, else False
-    """
-    keys = set([0] + boxes[0])
-    locked = set()
-    for box in boxes:
-        ibox = boxes.index(box)
-        if ibox not in keys:
-            if max(keys) > ibox:
-                locked.add(ibox)
-                continue
-        keys |= set(box)
-    for key in locked:
-        if key in keys:
-            keys |= set(boxes[key])
-    return not bool(locked - keys)
+    n = len(boxes)
+    unlocked = set([0])
+    keys = set(boxes[0])
+    
+    while keys:
+        key = keys.pop()
+        if key < n and key not in unlocked:
+            unlocked.add(key)
+            keys.update(boxes[key])
+            
+            return len(unlocked) == n
+
